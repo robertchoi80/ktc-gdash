7d95e6ce5042bb94fba1ddf632deac35d8d780bf
Robert <taeil.choi@kt.com>
2014-03-11 10:50:02 +0900

    set default column size to 3

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 include_attribute 'gdash::gdash'
 include_attribute 'graphite::default'
 
-default[:gdash][:columns] = 1
+default[:gdash][:columns] = 3
 default[:gdash][:monitor_client_recipe] = 'ktc-base'
